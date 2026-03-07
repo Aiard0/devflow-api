2b3582423aa30481646ddd41a747cb6aa42925e8
Aiard0 <aiard0yt3@gmail.com>
2026-03-07 09:49:33 -0300

    sixth phase

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from app.routes import example
 
 app = FastAPI(title="DevFlow API")
 
-
 @app.get("/", response_class=HTMLResponse)
 def read_root():
     html = (Path(__file__).parent / "static" / "index.html").read_text()
--- app/routes/example.py
+++ app/routes/example.py
@@ -5,3 +5,7 @@ router = APIRouter()
 @router.get("/ping")
 def ping():
     return {"ping": "pong"}
+
+@router.get("/test")
+def test():
+    return {"test": "success!"}
